929d7fbf84e8359d9b89b77fa8cb9173768f795d
jkdcam83@hotmail.com <root@primaria.(none)>
2013-05-28 10:32:56 +0200

    funcio resta afegida

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
             $suma = $variable1+$variable2;
             return $suma;
         }
+
+	function resta($variable1,$variable2){
+	$resta = $variable1-$variable2;
+	return $resta;
+	}
         ?>
     </body>
 </html>
